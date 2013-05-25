281f3a1fe3f93c08b74ad523e208f341bb890c60
niXman <i.nixman@gmail.com>
2013-05-25 14:31:35 +0400

    some properties added & some refactoring

--- variant.hpp
+++ variant.hpp
@@ -21,6 +21,7 @@ private:
 	struct i_type_holder {
 		virtual ~i_type_holder() {}
 
+		virtual bool is_empty() const = 0;
 		virtual bool is_integral() const = 0;
 		virtual bool is_signed() const = 0;
 		virtual bool is_unsigned() const = 0;
@@ -30,8 +31,10 @@ private:
 		virtual bool is_member_data_pointer() const = 0;
 		virtual bool is_member_function_pointer() const = 0;
 		virtual bool is_pod() const = 0;
-		virtual bool is_class() const = 0;
+		virtual bool is_array() const = 0;
+		virtual bool is_enum() const = 0;
 		virtual bool is_union() const = 0;
+		virtual bool is_class() const = 0;
 		virtual bool is_polymorphic() const = 0;
 
 		virtual std::size_t size_of() const = 0;
@@ -51,44 +54,50 @@ private:
 			:data_(v)
 		{}
 
-		virtual bool is_integral() const
+		bool is_empty() const
+			{ return std::is_empty<T>::value; }
+		bool is_integral() const
 			{ return std::is_integral<T>::value; }
-		virtual bool is_signed() const
+		bool is_signed() const
 			{ return std::is_signed<T>::value; }
-		virtual bool is_unsigned() const
+		bool is_unsigned() const
 			{ return std::is_unsigned<T>::value; }
-		virtual bool is_floating() const
+		bool is_floating() const
 			{ return std::is_floating_point<T>::value; }
-		virtual bool is_pointer() const
+		bool is_pointer() const
 			{ return std::is_pointer<T>::value; }
-		virtual bool is_function_pointer() const {
+		bool is_function_pointer() const {
 			return std::is_function<typename std::remove_pointer<T>::type>::value
 				&& std::is_pointer<T>::value;
 		}
-		virtual bool is_member_data_pointer() const
+		bool is_member_data_pointer() const
 			{ return std::is_member_object_pointer<T>::value; }
-		virtual bool is_member_function_pointer() const
+		bool is_member_function_pointer() const
 			{ return std::is_member_function_pointer<T>::value; }
-		virtual bool is_pod() const
+		bool is_pod() const
 			{ return std::is_pod<T>::value; }
-		virtual bool is_class() const
-			{ return std::is_class<T>::value; }
-		virtual bool is_union() const
+		bool is_array() const
+			{ return std::is_array<T>::value; }
+		bool is_enum() const
+			{ return std::is_enum<T>::value; }
+		bool is_union() const
 			{ return std::is_union<T>::value; }
-		virtual bool is_polymorphic() const
+		bool is_class() const
+			{ return std::is_class<T>::value; }
+		bool is_polymorphic() const
 			{ return std::is_polymorphic<T>::value; }
 
-		virtual std::size_t size_of() const
+		std::size_t size_of() const
 			{ return sizeof(T); }
-		virtual std::size_t alignment_of() const
+		std::size_t alignment_of() const
 			{ return std::alignment_of<T>::value; }
 
-		virtual const std::type_info& type_id() const
+		const std::type_info& type_id() const
 			{ return typeid(T); }
-		virtual const std::string type_name() const
+		const std::string type_name() const
 			{ return demangle(type_id()); }
 
-		virtual std::ostream& fmt_func(std::ostream& os) const
+		std::ostream& fmt_func(std::ostream& os) const
 			{ return (os << data_); }
 
 	private:
@@ -116,61 +125,65 @@ public:
 	{}
 	
 	bool is_integral() const
-		{ return data()->is_integral(); }
+		{ return data_->is_integral(); }
 	bool is_signed() const
-		{ return data()->is_signed(); }
+		{ return data_->is_signed(); }
 	bool is_unsigned() const
-		{ return data()->is_unsigned(); }
+		{ return data_->is_unsigned(); }
 	bool is_floating() const
-		{ return data()->is_floating(); }
+		{ return data_->is_floating(); }
 	bool is_pointer() const
-		{ return data()->is_pointer(); }
+		{ return data_->is_pointer(); }
 	bool is_function_pointer() const
-		{ return data()->is_function_pointer(); }
+		{ return data_->is_function_pointer(); }
 	bool is_member_data_pointer() const
-		{ return data()->is_member_data_pointer(); }
+		{ return data_->is_member_data_pointer(); }
 	bool is_member_function_pointer() const
-		{ return data()->is_member_function_pointer(); }
+		{ return data_->is_member_function_pointer(); }
 	bool is_pod() const
-		{ return data()->is_pod(); }
-	bool is_class() const
-		{ return data()->is_class(); }
+		{ return data_->is_pod(); }
+	bool is_array() const
+		{ return data_->is_array(); }
+	bool is_enum() const
+		{ return data_->is_enum(); }
 	bool is_union() const
-		{ return data()->is_union(); }
+		{ return data_->is_union(); }
+	bool is_class() const
+		{ return data_->is_class(); }
 	bool is_polymorphic() const
-		{ return data()->is_polymorphic(); }
+		{ return data_->is_polymorphic(); }
 
 	std::size_t size_of() const
-		{ return data()->size_of(); }
+		{ return data_->size_of(); }
 	std::size_t alignment_of() const
-		{ return data()->alignment_of(); }
+		{ return data_->alignment_of(); }
 
 	const std::type_info& type_id() const
-		{ return data()->type_id(); }
+		{ return data_->type_id(); }
 	const std::string type_name() const
-		{ return data()->type_name(); }
+		{ return data_->type_name(); }
 
 	template<typename T>
 	bool is_same() const
-		{ return (0 == strcmp(typeid(T).name(), data()->type_id().name())); }
+		{ return (0 == strcmp(typeid(T).name(), data_->type_id().name())); }
 
 	template<typename T>
 	T& get() {
-		if ( data()->is_pointer() ) {
+		if ( data_->is_pointer() ) {
 			if ( !std::is_pointer<T>::value ) { throw std::bad_cast(); }
-			return static_cast<type_holder<T>*>(data())->data_;
+			return static_cast<type_holder<T>*>(data_.get())->data_;
 		}
 		if ( !is_same<T>() ) { throw std::bad_cast(); }
-		return static_cast<type_holder<T>*>(data())->data_;
+		return static_cast<type_holder<T>*>(data_.get())->data_;
 	}
 	template<typename T>
 	const T& get() const {
-		if ( data()->is_pointer() ) {
+		if ( data_->is_pointer() ) {
 			if ( !std::is_pointer<T>::value ) { throw std::bad_cast(); }
-			return static_cast<type_holder<T>*>(data())->data_;
+			return static_cast<type_holder<T>*>(data_.get())->data_;
 		}
 		if ( !is_same<T>() ) { throw std::bad_cast(); }
-		return static_cast<type_holder<T>*>(data())->data_;
+		return static_cast<type_holder<T>*>(data_.get())->data_;
 	}
 
 	std::ostream& dump(std::ostream& os) const {
@@ -191,18 +204,17 @@ public:
 			<< "is_member_data_pointer    : " << is_member_data_pointer() << std::endl
 			<< "is_member_function_pointer: " << is_member_function_pointer() << std::endl
 			<< "is_pod                    : " << is_pod() << std::endl
-			<< "is_class                  : " << is_class() << std::endl
+			<< "is_array                  : " << is_array() << std::endl
+			<< "is_enum                   : " << is_enum() << std::endl
 			<< "is_union                  : " << is_union() << std::endl
+			<< "is_class                  : " << is_class() << std::endl
 			<< "is_polymorphic            : " << is_polymorphic() << std::endl
 		;
 	}
 
 private:
 	friend std::ostream& operator<< (std::ostream& os, const variant& v)
-		{ return v.data()->fmt_func(os); }
-
-	i_type_holder* data() const
-		{ return data_?data_.get():throw std::runtime_error(__PRETTY_FUNCTION__); }
+		{ return v.data_->fmt_func(os); }
 
 	std::shared_ptr<i_type_holder> data_;
 };
